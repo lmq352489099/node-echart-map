734a2f8b2358016a7a16442a25b605d760053ef8
lmq4088 <352489099@qq.com>
2020-04-09 16:44:04 +0800

    12

--- index.js
+++ index.js
@@ -16,11 +16,32 @@ app.all('*', function (req, res, next) {
   res.header("X-Powered-By", ' 3.2.1');
   next();
 });
+// app.use(express.static("./map.html"));
+app.use('/js', express.static(__dirname + '/js'));
+app.use('/node_modules', express.static(__dirname + '/node_modules'));
+
+app.get('/123', function (req, res) {
+  console.log("进来了");
+
+  //response.writeHead(响应状态码，响应头对象): 发送一个响应头给请求。
+  // res.writeHead(200, { 'Content-Type': 'text/html' })
+  // 如果url=‘/’ ,读取指定文件下的html文件，渲染到页面。
+  fs.readFile('./map.html', 'utf-8', function (err, data) {
+    if (err) {
+      throw err;
+    }
+
+    res.send(data);
+  })
+
+})
+
+
+
 
 app.get('/', function (req, res) {
   console.log("进来了");
-
-
+  // res.render('./map.html');
   requests('https://ncov.dxy.cn/ncovh5/view/pneumonia?from=timeline&isappinstalled=0').on('data', (chunk) => {
 
     let window = {}
@@ -28,7 +49,7 @@ app.get('/', function (req, res) {
     eval($('#getAreaStat').html())
     console.log(window);
 
-  // res.send(JSON.stringify(window.getAreaStat))
+    // res.send(JSON.stringify(window.getAreaStat))
     // fs.writeFile(path.resolve(__dirname, 'data.json'), JSON.stringify(window.getAreaStat), (err) => {
     //   // if (err) {
     //   //   res.send('失败')
@@ -43,8 +64,8 @@ app.get('/', function (req, res) {
 
 
 
-  fs.writeFileSync(path.resolve(__dirname, 'data.json'), JSON.stringify(window.getAreaStat));
-     res.send(JSON.stringify(window.getAreaStat))
+    // fs.writeFileSync(path.resolve(__dirname, 'data.json'), JSON.stringify(window.getAreaStat));
+    res.send(JSON.stringify(window.getAreaStat))
   })
 
 
